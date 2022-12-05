723b13e0e77ec6d43624f445d8e957b7e2cae5d0
Hayden <mathsfreak628@hotmail.com>
2022-12-05 15:17:55 +0800

    Refinements to theUsualSuspects.py

--- theUsualSuspects.py
+++ theUsualSuspects.py
@@ -4,7 +4,7 @@ with open('GRBGCNTimesDatabase.txt','r') as f:
     s = f.read()
     burstDict = eval(s)
 
-suspectList = [] #each entry is a list of the form [GRB, circular, log10(delta_t)]
+suspectList = [] #each entry is a list of the form [GRB, offenceCount, circular, log10(delta_t)]
 for key in burstDict:
     for circ in burstDict[key]:
         suspect = [key, circ[0],np.log10(circ[2])]
@@ -13,7 +13,29 @@ for key in burstDict:
 suspectList.sort(key=lambda suspect: suspect[2])
 suspectList.reverse()
 
+topSuspects = suspectList[:1000]
+for suspect in topSuspects:
+    count = 0
+    burst = suspect[0]
+    for otherSuspect in topSuspects:
+        if burst == otherSuspect[0]:
+            count += 1
+    suspect.append(count)
+
+
 i = 0
+print('Burst, offending circular, log_10(delta_t), number of circs in top 1000')
 while i < 100:
-    print(suspectList[i])
+    print(topSuspects[i])
     i += 1
+
+topOffenders = []
+for suspect in topSuspects:
+    if suspect[3] > 1 and not suspect[0] in topOffenders:
+        topOffenders.append(suspect[0])
+
+print('Ordered by number of circulars in top 1000')
+topSuspects.sort(key=lambda suspect: suspect[3])
+topSuspects.reverse()
+for suspect in topSuspects:
+    print(suspect)
